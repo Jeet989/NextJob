9cfe3b9acd7c6cc7cfc3a6d89be81fbf0f97fa7d
JEET MEHTA <jt429386@dal.ca>
2023-08-23 17:40:32 -0300

    added the backend of the project

---
+++ backend/models/payload/usersPayload.go
@@ -0,0 +1,30 @@
+/*
+Author: Jeet Mehta
+Banner ID: B00945900
+Email ID: jt429386@dal.ca
+*/
+
+package payload
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type CreateUserPayload struct {
+	FirstName string `json:"firstName" binding:"required,alpha" msg:"First Name must contain alphabets only"`
+	LastName  string `json:"lastName" binding:"required,alpha" msg:"Last Name must contain alphabets only"`
+	Email     string `json:"email" binding:"required,email" msg:"Invalid Email Address"`
+	Password  string `json:"password" binding:"required,alphanum" msg:"Password must contain alphabets and numbers only"`
+}
+
+type LoginUserPayload struct {
+	Email     string `json:"email" binding:"required,email" msg:"Invalid Email Address"`
+	Password  string `json:"password"`
+	LoginType string `json:"loginType"`
+}
+
+type GetUser struct {
+	FirstName string             `json:"firstName"`
+	LastName  string             `json:"lastName"`
+	Email     string             `json:"email"`
+	Password  string             `json:"password"`
+	Id        primitive.ObjectID `json:"id,omitempty"`
+}
---
+++ backend/utils/GenerateJWT.go
@@ -0,0 +1,42 @@
+/*
+Author: Jeet Mehta
+Banner ID: B00945900
+Email ID: jt429386@dal.ca
+*/
+
+package utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func GenerateJWT(email string) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		email: email,
+		"exp": time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return tokenString
+}
+
+// verify token
+func VerifyToken(tokenString string) bool {
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	return true
+}
