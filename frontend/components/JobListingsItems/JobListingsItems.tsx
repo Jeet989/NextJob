f43192ad4ef8edda97b542fe8b2a7c86451f62e0
JEET MEHTA <jt429386@dal.ca>
2023-08-23 17:43:32 -0300

    added the frontend part of the project

---
+++ frontend/components/JobListingsItems/JobListingsItems.tsx
@@ -0,0 +1,29 @@
+/*
+Author: Kishan Patel
+Banner ID : B00929225
+Email ID: ks255715@dal.ca
+*/
+
+"use client";
+
+import { Grid } from "@mui/material";
+import JobCard from "../JobCard/JobCard";
+import { JobListingsItemsProps } from "@/app/job-listings/types";
+
+const JobListingsItems = ({ jobData }: JobListingsItemsProps) => {
+  return (
+    <Grid container>
+      {jobData?.map((card, index) => {
+        return (
+          card.jobStatus === "Active" && (
+            <Grid item xs={12} sm={12} md={6} lg={4} key={index}>
+              <JobCard key={index} {...card} />
+            </Grid>
+          )
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default JobListingsItems;
